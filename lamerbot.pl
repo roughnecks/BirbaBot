620eb0190221b3d39cd4ea4a125b3319fc78022a
Marco <marco@theanarchistlibrary.org>
2011-10-04 19:31:51 +0200

    Let's the bot connect
    Ok, I pasted some code from the POE::Component::IRC manual.
    Just to see if the thing can actually work. And it works.
    
      *  edited              example.conf
      *  edited              lamerbot.pl

--- lamerbot.pl
+++ lamerbot.pl
@@ -12,12 +12,115 @@ use warnings;
 use Data::Dumper;
 
 my $config_file = $ARGV[0];
+my $debug = $ARGV[1];
 
 show_help() unless $config_file;
 
+### configuration checking 
+
 my $configuration = read_config($config_file);
 
-print Dumper($configuration);
+print Dumper($configuration) if $debug;
+
+my $nick = $configuration->{'nick'};
+my $ircname = $configuration->{'ircname'};
+my $server = $configuration->{'server'};
+
+show_help("Missing configuration options\n") 
+  unless ($configuration->{nick} and
+	$configuration->{ircname} and 
+	$configuration->{server});
+
+my @channels = split(/ *, */, $configuration->{'channels'});
+
+
+if ($debug) {
+  print "Connecting to $server with nick $nick and ircname $ircname, ",
+    "joining channels ", 
+      join(" and ", @channels), "\n";
+}
+
+### starting POE stuff
+
+use POE qw(Component::IRC);
+
+my $irc = POE::Component::IRC->spawn(
+				     # TODO: before using this
+				     # directly would be better to
+				     # clean it up, ok?
+				     %$configuration
+) or die "WTF? $!\n";
+
+POE::Session->create(
+		     package_states => [
+					main => [qw(_default 
+						    _start 
+						    irc_001
+						    irc_public)],
+				       ],
+		     heap => {irc => $irc},
+		     );
+$poe_kernel->run();
+
+
+## just copy and pasted, ok?
+
+sub _start {
+    my $heap = $_[HEAP];
+
+    # retrieve our component's object from the heap where we stashed it
+    my $irc = $heap->{irc};
+
+    $irc->yield( register => 'all' );
+    $irc->yield( connect => { } );
+    return;
+}
+
+sub irc_001 {
+    my $sender = $_[SENDER];
+
+    # Since this is an irc_* event, we can get the component's object by
+    # accessing the heap of the sender. Then we register and connect to the
+    # specified server.
+    my $irc = $sender->get_heap();
+
+    print "Connected to ", $irc->server_name(), "\n";
+
+    # we join our channels
+    $irc->yield( join => $_ ) for @channels;
+    return;
+}
+
+sub irc_public {
+    my ($sender, $who, $where, $what) = @_[SENDER, ARG0 .. ARG2];
+    my $nick = ( split /!/, $who )[0];
+    my $channel = $where->[0];
+
+    if ( my ($rot13) = $what =~ /^rot13 (.+)/ ) {
+        $rot13 =~ tr[a-zA-Z][n-za-mN-ZA-M];
+        $irc->yield( privmsg => $channel => "$nick: $rot13" );
+    }
+    return;
+}
+
+# We registered for all events, this will produce some debug info.
+sub _default {
+    my ($event, $args) = @_[ARG0 .. $#_];
+    my @output = ( "$event: " );
+
+    for my $arg (@$args) {
+        if ( ref $arg eq 'ARRAY' ) {
+            push( @output, '[' . join(', ', @$arg ) . ']' );
+        }
+        else {
+            push ( @output, "'$arg'" );
+        }
+    }
+    print join ' ', @output, "\n";
+    return 0;
+}
+
+
 
 exit;
 
@@ -45,7 +148,9 @@ sub read_config {
       my $value = $2;
       $value =~ s/^"//;
       $value =~ s/"$//;
-      $config{$1} = $2;
+      if ($key && $value) {
+	$config{$key} = $value;
+      }
     }
   }
   close $fh;
@@ -60,7 +165,11 @@ Well, show the help and exit
 
 
 sub show_help {
+  my $string = shift;
+  print "$string\n" if $string;
+
   print <<HELP;
+
 
 This script runs the bot. It takes a mandatory argument with the
 configuration file. The configuration file have values like this:
