d45c48b6303defb3977eaad68cbfbfd080b74c3a
roughnecks <simcana@gmail.com>
2011-10-05 16:50:37 +0200

    new sql: date fixes

--- sqlprototype.sql
+++ sqlprototype.sql
@@ -9,7 +9,7 @@
 PRAGMA foreign_keys = ON;
 CREATE TABLE IF NOT EXISTS rss (
         r_id    	INTEGER PRIMARY KEY,
-	date		DATETIME,
+	date		DATE,
 	f_handle	VARCHAR(255),
 	f_channel	VARCHAR(30),
         url     	TEXT,
@@ -21,9 +21,9 @@ CREATE TABLE IF NOT EXISTS rss (
 --- Load some sample data
 ---
 
-INSERT INTO rss VALUES (NULL, DATETIME('NOW'), 'laltrowiki', '#l_altro_mondo', 'http://laltromondo.dynalias.net/~iki/recentchanges/index.rss', 1);
-INSERT INTO rss VALUES (NULL, '2011-10-05 00:11:00', 'lamerbot', '#l_altro_mondo', 'http://laltromondo.dynalias.net/gitweb/?p=lamerbot.git;a=rss', 1);
-INSERT INTO rss VALUES (NULL, '2011-10-05 00:11:00', 'lamerbot', '#lamerbot', 'http://laltromondo.dynalias.net/gitweb/?p=lamerbot.git;a=rss', 0);
+INSERT INTO rss VALUES (NULL, DATE('NOW'), 'laltrowiki', '#l_altro_mondo', 'http://laltromondo.dynalias.net/~iki/recentchanges/index.rss', 1);
+INSERT INTO rss VALUES (NULL, '2011-10-04', 'lamerbot', '#l_altro_mondo', 'http://laltromondo.dynalias.net/gitweb/?p=lamerbot.git;a=rss', 1);
+INSERT INTO rss VALUES (NULL, '2011-10-05', 'lamerbot', '#lamerbot', 'http://laltromondo.dynalias.net/gitweb/?p=lamerbot.git;a=rss', 0);
 
 --- SECTION 3: documented queries
 
@@ -48,6 +48,7 @@ SELECT f_handle FROM rss WHERE f_channel='#l_altro_mondo' AND active=1;
 
 CREATE TABLE IF NOT EXISTS feed_laltromondo (
         id          	INTEGER PRIMARY KEY,
+	date		DATETIME,
         title	    	VARCHAR(255),
         author		VARCHAR(255),
 	url	    	TEXT UNIQUE,
@@ -58,12 +59,12 @@ CREATE TABLE IF NOT EXISTS feed_laltromondo (
 --- Load some sample data
 ---
 
-INSERT INTO feed_laltromondo VALUES (NULL, 'first commit', 'rough', 'http://laltromondo.dynalias.net/gitweb?p=LAltroWiki.git;a=blobdiff;01', 'blahblahdata1');
+INSERT INTO feed_laltromondo VALUES (NULL, DATETIME('NOW'), 'first commit', 'rough', 'http://laltromondo.dynalias.net/gitweb?p=LAltroWiki.git;a=blobdiff;01', 'blahblahdata1');
 SELECT '
 here there is a wanted error caused by the url in the next INSERT statement wich is not unique
 ';
-INSERT INTO feed_laltromondo VALUES (NULL, 'second commit', 'rough', 'http://laltromondo.dynalias.net/gitweb?p=LAltroWiki.git;a=blobdiff;01', 'blahblahdata2');
-INSERT INTO feed_laltromondo VALUES (NULL, 'third commit', 'melmoth', 'http://laltromondo.dynalias.net/gitweb?p=LAltroWiki.git;a=blobdiff;03', 'blahblahdata3');
+INSERT INTO feed_laltromondo VALUES (NULL, DATETIME('NOW'), 'second commit', 'rough', 'http://laltromondo.dynalias.net/gitweb?p=LAltroWiki.git;a=blobdiff;01', 'blahblahdata2');
+INSERT INTO feed_laltromondo VALUES (NULL, '2011-10-04 20:17:00', 'third commit', 'melmoth', 'http://laltromondo.dynalias.net/gitweb?p=LAltroWiki.git;a=blobdiff;03', 'blahblahdata3');
 
 
 --- SECTION 3: documented queries for feeds TABLES
