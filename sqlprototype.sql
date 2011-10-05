78df3fed7fff7223296621e49b2137131f8fa1c9
roughnecks <simcana@gmail.com>
2011-10-05 11:28:09 +0200

    sqlprototype: f_channel, active, comments and query

--- sqlprototype.sql
+++ sqlprototype.sql
@@ -3,18 +3,21 @@
 --
 
 -- TABLE which will store the feeds' items list. (rss)
---
+-- r_id is the rss id number (incremental); f_handle is the friendly name of the feed; "active" tells if the the feed is currently watched on f_channel. 
+-- If the same feed is used in multiple channels, we use two different records with all the same columns and different channel/active values.
 
 PRAGMA foreign_keys = ON;
 CREATE TABLE rss (
         r_id    	INTEGER PRIMARY KEY,
 	date		DATETIME,
-	f_handle	varchar(255),
-        url     	TEXT
+	f_handle	VARCHAR(255),
+	f_channel	VARCHAR(30),
+        url     	TEXT,
+        active		BOOLEAN
 );
 
--- EXAMPLE TABLE which will store the content of each feed item. (feed_name)
---
+-- EXAMPLE TABLE which will store the content of each feed item. (feed_handle)
+-- f_id is the feed id number (incremental); f_handle is the same as in the rss table.
 
 CREATE TABLE feed_laltromondo (
         f_id          	INTEGER PRIMARY KEY,
@@ -43,25 +46,27 @@ CREATE TABLE feed_lamerbot (
 --- Load some sample data
 ---
 
-INSERT INTO rss VALUES (NULL, '2011-10-05 00:10:00', 'laltrowiki', 'http://laltromondo.dynalias.net/~iki/recentchanges/index.rss');
-INSERT INTO rss VALUES (NULL, '2011-10-05 00:11:00', 'lamerbot', 'http://laltromondo.dynalias.net/gitweb/?p=lamerbot.git;a=rss');
+INSERT INTO rss VALUES (NULL, '2011-10-05 00:10:00', 'laltrowiki', '#l_altromondo', 'http://laltromondo.dynalias.net/~iki/recentchanges/index.rss', 1);
+INSERT INTO rss VALUES (NULL, '2011-10-05 00:11:00', 'lamerbot', '#l_altro_mondo', 'http://laltromondo.dynalias.net/gitweb/?p=lamerbot.git;a=rss', 1);
+INSERT INTO rss VALUES (NULL, '2011-10-05 00:11:00', 'lamerbot', '#lamerbot', 'http://laltromondo.dynalias.net/gitweb/?p=lamerbot.git;a=rss', 0);
 
 INSERT INTO feed_laltromondo VALUES (NULL, '2011-10-05 00:12:00', 'laltromondo', 'first commit', 'rough', 'http://laltromondo.dynalias.net/gitweb?p=LAltroWiki.git;a=blobdiff;f', 'blahblahdata1', 'blahblahbody1');
 INSERT INTO feed_laltromondo VALUES (NULL, '2011-10-05 00:13:00', 'laltromondo', 'second commit', 'rough', 'http://laltromondo.dynalias.net/gitweb?p=LAltroWiki.git;a=blobdiff;', 'blahblahdata2', 'blahblahbody2');
+INSERT INTO feed_laltromondo VALUES (NULL, '2011-10-06 00:13:00', 'laltromondo', 'third commit', 'melmoth', 'http://laltromondo.dynalias.net/gitweb?p=LAltroWiki.git;a=blobdiff;', 'blahblahdata3', 'blahblahbody3');
 
 INSERT INTO feed_lamerbot VALUES (NULL, '2011-10-05 00:15:00', 'lamerbot', 'first commit', 'melmothx', 'http://laltromondo.dynalias.net/gitweb?p=LAltroWiki.git;a=blobdiff;f=in', 'blahblahdata3', 'blahblahbody3');
 INSERT INTO feed_lamerbot VALUES (NULL, '2011-10-05 00:16:00', 'lamerbot', 'second commit', 'rough', 'http://laltromondo.dynalias.net/gitweb?p=LAltroWiki.git;a=blobdiff;f=info', 'blahblahdata4', 'blahblahbody5');
 
 --- SECTION 3: documented queries
 
--- query to see which urls we need to fetch
-SELECT url FROM rss;
+-- query to see which urls we need to fetch for which channel
+SELECT url,f_channel FROM rss WHERE active=1;
 
--- query a feed_name to get the last fetched item id
+-- query a feed_name to get the last fetched item id for feed_handle
 SELECT f_id FROM feed_laltromondo ORDER BY f_id DESC LIMIT 1;
 
--- query a feed_name to get the last 2 fetched items (from all tables)
-SELECT * FROM feed_laltromondo ORDER BY f_id LIMIT 2;
+-- query a feed_name to get the last 2 fetched items for feed_handle (from all tables)
+SELECT * FROM feed_laltromondo ORDER BY f_id DESC LIMIT 2;
 
--- query a feed_name to get the last 2 fetched items (from selected tables)
-SELECT f_handle,title,url FROM feed_laltromondo ORDER BY f_id LIMIT 2;
+-- query a feed_name to get the last 2 fetched items for feed_handle (from selected tables)
+SELECT f_handle,title,url FROM feed_laltromondo ORDER BY f_id DESC LIMIT 2;
