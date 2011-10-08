837361958cebdca6aa7264c739d6e3d80e5f51f1
roughnecks <info@laltromondo.dynalias.net>
2011-10-08 23:47:59 +0200

    fix myfeeds.sql

---
+++ sql/myfeeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO rss VALUES ('laltroikiw', 'http://laltromondo.dynalias.net/~iki/recentchanges/index.rss');
+INSERT INTO rss VALUES ('birbabot', 'http://laltromondo.dynalias.net/gitweb/?p=lamerbot.git;a=rss');
+INSERT INTO rss VALUES ('slashdot', 'http://rss.slashdot.org/Slashdot/slashdot');
+INSERT INTO rss VALUES ('ansa', 'http://ansa.it/web/notizie/rubriche/topnews/topnews_rss.xml');
+INSERT INTO rss VALUES ('boingboing', 'http://feeds.boingboing.net/boingboing/iBag');
+
+INSERT INTO channels VALUES ('slashdot', '#l_altro_mondo');
+INSERT INTO channels VALUES ('boingboing', '#l_altro_mondo');
+INSERT INTO channels VALUES ('ansa', '#l_altro_mondo');
+INSERT INTO channels VALUES ('laltroikiw', '#l_altro_mondo');
+INSERT INTO channels VALUES ('birbabot', '#l_altro_mondo');
