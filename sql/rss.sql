79fee48d2f06ad1055fa49228ab279ff52030263
roughnecks <info@laltromondo.dynalias.net>
2011-10-11 19:24:44 +0200

    select last N feeds

--- sql/rss.sql
+++ sql/rss.sql
@@ -20,3 +20,17 @@ CREATE TABLE IF NOT EXISTS feeds (
         url                     TEXT UNIQUE NOT NULL,
         FOREIGN KEY(f_handle) REFERENCES rss(f_handle) ON DELETE CASCADE
 );
+
+
+SELECT '
+query per pescare gli ultimi 5 feed 
+';
+
+SELECT f_handle,title,url FROM feeds ORDER BY id DESC LIMIT 5;
+
+SELECT '
+query per pescare gli ultimi 5 feed per ansa
+';
+
+SELECT f_handle,title,url FROM feeds WHERE f_handle='ansa' ORDER BY id DESC LIMIT 5;
+
