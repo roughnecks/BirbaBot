32bd285b0c973363fd87896cd180092a4b7c68e0
Marco <marco@theanarchistlibrary.org>
2011-10-14 09:55:29 +0200

    Moved some general purpose functions to BirbaBot.pm
    use BirbaBot qw(create_bot_db
    		read_config
    		override_defaults show_help);
    
    The purpose is to keep the birbabot.pl as much clean as possible, with
    only the irc-related things inside
    
      *  edited              birbabot.pl
      *  edited              BirbaBot/lib/BirbaBot.pm

--- BirbaBot/lib/BirbaBot.pm
+++ BirbaBot/lib/BirbaBot.pm
@@ -14,7 +14,9 @@ our @ISA = qw(Exporter);
 # names by default without a very good reason. Use EXPORT_OK instead.
 # Do not simply export all your public functions/methods/constants.
 
-our @EXPORT_OK = qw(create_bot_db);
+our @EXPORT_OK = qw(create_bot_db
+		read_config
+		override_defaults show_help);
 
 our $VERSION = '0.01';
 
@@ -61,6 +63,82 @@ sub create_bot_db {
   $dbh->disconnect;
   return 1;
 }
+
+=head2 read_config
+
+Read the configuration file, which should be passed on the command
+line, and return an hashref with key-value pairs for the bot config.
+
+=cut 
+
+sub read_config {
+  my $file = shift;
+  my %config;
+  show_help() unless (-f $file);
+  open (my $fh, "<", $file) or die "Cannot read $file $!\n";
+  while (<$fh>) {
+    my $line = $_;
+    # strip whitespace
+    chomp $line;
+    $line =~ s/^\s+//;
+    $line =~ s/\s+$//;
+    next if ($line =~ m/^#/);
+    if ($line =~ m/(\w+)\s*=\s*(.*)/) {
+      my $key = $1;
+      my $value = $2;
+      $value =~ s/^"//;
+      $value =~ s/"$//;
+      if ($key && (defined $value)) {
+	$config{$key} = $value;
+      }
+    }
+  }
+  close $fh;
+  return \%config;
+}
+
+sub override_defaults {
+  # here we modify the values via reference on the fly
+  my ($default, $fromfile) = @_;
+  foreach my $key (keys(%$default)) {
+    if (exists $fromfile->{$key}) {
+      $default->{$key} = $fromfile->{$key};
+    }
+  }
+}
+
+
+=head2 show_help
+
+Well, show the help and exit
+
+=cut
+
+
+
+sub show_help {
+  my $string = shift;
+  print "$string\n" if $string;
+
+  print <<HELP;
+
+
+This script runs the bot. It takes a mandatory argument with the
+configuration file. The configuration file have values like this:
+
+nick = pippo_il_bot
+server = irc.syrolnet.org
+
+Blank lines are just ignored. Lines starting with # are ignored too.
+Don't put comments on the same line of a key-value pair, OK?
+(Otherwise the comment will become part of the value).
+
+Good luck.
+
+HELP
+  exit 1;
+}
+
 
 
 
--- birbabot.pl
+++ birbabot.pl
@@ -14,7 +14,9 @@ use File::Spec;
 use File::Path qw(make_path);
 use Data::Dumper;
 use lib './BirbaBot/lib';
-use BirbaBot qw(create_bot_db);
+use BirbaBot qw(create_bot_db
+		read_config
+		override_defaults show_help);
 use BirbaBot::RSS qw(
 		     rss_add_new
 		     rss_delete_feed
@@ -381,77 +383,3 @@ sub _default {
 
 exit;
 
-=head2 read_config
-
-Read the configuration file, which should be passed on the command
-line, and return an hashref with key-value pairs for the bot config.
-
-=cut 
-
-sub read_config {
-  my $file = shift;
-  my %config;
-  show_help() unless (-f $file);
-  open (my $fh, "<", $file) or die "Cannot read $file $!\n";
-  while (<$fh>) {
-    my $line = $_;
-    # strip whitespace
-    chomp $line;
-    $line =~ s/^\s+//;
-    $line =~ s/\s+$//;
-    next if ($line =~ m/^#/);
-    if ($line =~ m/(\w+)\s*=\s*(.*)/) {
-      my $key = $1;
-      my $value = $2;
-      $value =~ s/^"//;
-      $value =~ s/"$//;
-      if ($key && (defined $value)) {
-	$config{$key} = $value;
-      }
-    }
-  }
-  close $fh;
-  return \%config;
-}
-
-sub override_defaults {
-  # here we modify the values via reference on the fly
-  my ($default, $fromfile) = @_;
-  foreach my $key (keys(%$default)) {
-    if (exists $fromfile->{$key}) {
-      $default->{$key} = $fromfile->{$key};
-    }
-  }
-}
-
-
-=head2 show_help
-
-Well, show the help and exit
-
-=cut
-
-
-
-sub show_help {
-  my $string = shift;
-  print "$string\n" if $string;
-
-  print <<HELP;
-
-
-This script runs the bot. It takes a mandatory argument with the
-configuration file. The configuration file have values like this:
-
-nick = pippo_il_bot
-server = irc.syrolnet.org
-
-Blank lines are just ignored. Lines starting with # are ignored too.
-Don't put comments on the same line of a key-value pair, OK?
-(Otherwise the comment will become part of the value).
-
-Good luck.
-
-HELP
-  exit 1;
-}
