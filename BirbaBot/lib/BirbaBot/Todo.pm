aa8c8e9258ea253fef1f11e59bceab230ab46ca1
Marco <marco@theanarchistlibrary.org>
2011-10-16 16:40:42 +0200

    Added doc e function stubs

--- BirbaBot/lib/BirbaBot/Todo.pm
+++ BirbaBot/lib/BirbaBot/Todo.pm
@@ -18,6 +18,57 @@ our @EXPORT_OK = qw(todo_add todo_remove todo_rearrange);
 
 our $VERSION = '0.01';
 
+=head2 todo_add($dbname, $channel, $todo)
+
+Add the string $todo to $dbname for the channel $channel. Return a
+string telling what we did
+
+=cut
+
+sub todo_add {
+  my ($dbname, $channel, $todo);
+  return "Not implemented, yet"; 
+}
+
+=head2 todo_remove($dbname, $channel, $id)
+
+Remove the field with id $id from $dbname for the channel $channel. Return a
+string telling what we did.
+
+=cut
+
+sub todo_remove {
+  my ($dbname, $channel, $id);
+  return "Not implemented, yet"; 
+}
+
+
+
+
+=head2 todo_list($dbname, $channel)
+
+List the values in $dbname for channel $channel. Return the todos.
+
+=cut
+
+sub todo_list {
+  my ($dbname, $channel);
+  return "Not implemented, yet"; 
+}
+
+
+=head2 todo_rearrange($dbname, $channel)
+
+Rearrange the fields in $dbname for $channel, to get progressive ids.
+Return the new list of todo.
+
+=cut
+
+sub todo_remove {
+  my ($dbname, $channel);
+  return "Not implemented, yet"; 
+}
+
 
 
 
