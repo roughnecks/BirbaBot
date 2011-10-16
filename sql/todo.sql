ababbe8065ed2ddd8549b2f616057880c036ecd5
roughnecks <info@laltromondo.dynalias.net>
2011-10-16 14:32:54 +0200

    second todo.sql

--- sql/todo.sql
+++ sql/todo.sql
@@ -16,10 +16,37 @@ quindi per fare l insert bisogna aggiungere 1 (+1) al valore restituito dal sele
 
 SELECT MAX(id) FROM todo WHERE chan='#l_altro_mondo';
 
-INSERT INTO todo VALUES ('1', '#l_altro_mondo', 'first todo');
+SELECT '
+canale vuoto di todo, maxid per #l_altro_mondo nullo
+a questo punto il perl memorizza il valore restitutito, che sarà 0
+e si prosegue con l insert
+';
 
-SELECT MAX(id) FROM todo WHERE chan='#l_altro_mondo';
+--INSERT INTO todo VALUES ('1', '#l_altro_mondo', 'first todo');
+
+
+
+--SELECT MAX(id) FROM todo WHERE chan='#l_altro_mondo';
 
 INSERT INTO todo VALUES ('var_maxid +1', '#l_altro_mondo', 'second todo');
 
 SELECT * FROM todo;
+
+SELECT '
+ogni nuovo insert funziona come sopra, si fa il select >> maxid in variable, poi l insert con $var+1
+';
+
+SELECT '
+i delete sono semplici: si cancella per id su #canale
+';
+
+DELETE FROM todo WHERE chan='#l_altro_mondo' AND id='numero';
+
+SELECT '
+a questo punto abbiamo ogni canale con la sua numerazione che parte sempre da 1
+il problema è quando coi delete "spezziamo" l ordinamento: 1 3 4 5 8
+finché si lascia spezzato il delete funziona normalmente, è solo brutto a vedersi
+quindi, a richiesta bisogna poter riordinare il tutto
+qui entri in gioco tu, perché non so dove mettere le mali col sql
+';
+
