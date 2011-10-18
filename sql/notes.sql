f319e7bc58843a36cfb3729f3d284e735b315a25
roughnecks <info@laltromondo.dynalias.net>
2011-10-18 11:55:21 +0200

    fix to notes.sql

--- sql/notes.sql
+++ sql/notes.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS notes (
-       date  	    DATETIME,
+       date  	    VARCHAR(150),
        sender	    VARCHAR(30),
        receiver	    VARCHAR(30) NOT NULL,
        message	    TEXT NOT NULL
@@ -9,3 +9,4 @@ SELECT sender,message,date FROM notes WHERE receiver='';
 
 DELETE FROM notes WHERE receiver='';
 
+INSERT INTO notes (date, sender, receiver, message) VALUES ( ,'' ,'' ,'');
