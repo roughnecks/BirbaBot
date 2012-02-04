200a6832fad8e426dbd63064d07c124f6a571fb6
roughnecks <roughnecks@laltromondo.dynalias.net>
2012-02-04 19:12:42 +0100

    remind plugin addedd

--- birbabot.pl
+++ birbabot.pl
@@ -153,6 +153,7 @@ POE::Session->create(
 		     irc_socketerr
 		     irc_ping
 		     irc_kick
+		     irc_botcmd_remind
 		     irc_botcmd_version
 		     irc_botcmd_choose
 		     irc_botcmd_bash
@@ -210,7 +211,7 @@ sub _start {
             note => 'Send a note to a user: note <nick> <message>',
             todo => 'add something to the channel TODO; todo [ add "foo" | rearrange | done #id ] - done < #id > ',
             done => 'delete something to the channel TODO; done #id ',
-
+	    remind => 'Store an alarm for the current user, delayed by "#n seconds" | remind <time> <message>',
             kw => 'Manage the keywords: kw foo is bar; kw foo is also bar2/3; kw forget foo; kw delete foo 2/3; kw => gives you the facts list',
             x => 'Translate some text from lang to lang (where language is a two digit country code), for example: "x en it this is a test".',
 	    meteo => 'Query the weather for location',							       
@@ -970,6 +971,15 @@ sub irc_botcmd_version {
 	exec "git", "status" or die "Can't exec git: $!";
       }
     }
+
+sub irc_botcmd_remind {
+  my ($who, $where, $what) = @_[ARG0..$#_];
+  my $nick = parse_user($who);
+  my @args = split(/ +/, $what);
+  my $time = shift(@args);
+  my $string = join (" ", @args);
+  $irc->delay ( [ privmsg => $where => "$nick, it's time to: $string" ], $time );
+}
 
 
 exit;
