f4940b43612bcc13a0fd276db6093517151f212f
roughnecks <roughnecks@laltromondo.dynalias.net>
2012-03-09 14:47:58 +0100

    limit sitedown queries to channels

--- birbabot.pl
+++ birbabot.pl
@@ -1166,6 +1166,7 @@ sub irc_botcmd_wikiz {
 
 sub irc_botcmd_sitedown {
   my ($where, $what) = @_[ARG1, ARG2];
+  return unless is_where_a_channel($where);
   return unless $what =~ m/^\s*(\w.+\.)+[a-z]{2,3}$/;
   my $prepend = 'http://www.downforeveryoneorjustme.com/';
   my $query = $prepend . $what;
