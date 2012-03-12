4b0d99677fb1fdd245471455c8db9f681aea4207
Marco <marco@theanarchistlibrary.org>
2012-03-12 22:21:19 +0100

    Fixes to the restart.bot.sh

--- restart.bot.sh
+++ restart.bot.sh
@@ -7,14 +7,14 @@ fi
 botdir=$(dirname $0)
 
 if [ ! -d "$botdir/logs" ]; then
-	mkdir "$botdir/logs"
+	mkdir -p "$botdir/logs"
 fi
 
 logfile=$botdir/logs/birba-`date +%F`.log
 
 if [ ! -f "$botdir/birba.pid" ]; then
     cd $botdir
-    exec perl birbabot.pl >> "$logfile" 2>&1
+    exec nohup perl birbabot.pl $1 >> "$logfile" 2>&1
 fi
 
 pid=$(cat $botdir/birba.pid)
@@ -25,6 +25,5 @@ else
     cd $botdir
     echo -n $(date) >> "$logfile"
     echo "Bot restarted by $0" >> "$logfile"
-    perl birbabot.pl $1 >> "$logfile" 2>&1 &
-    exit 0
+    exec nohup perl birbabot.pl $1 >> "$logfile" 2>&1 &
 fi
