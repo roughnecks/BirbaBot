1c770e7ff19dac11fc70c4018310838db85b0357
roughnecks <roughnecks@laltromondo.dynalias.net>
2012-08-05 12:33:42 +0200

    dpkg_facts-import.pl improvements and README

--- utils/dpkg_facts-import.pl
+++ utils/dpkg_facts-import.pl
@@ -7,22 +7,44 @@ use strict;
 use Data::Dumper qw(Dumper);
 use DBIx::Class;
 use DBI;
+use Cwd;
 
 # app::Schema is under utils/app/
 use app::Schema;
 
-# Prepare the connection handle
+my $cwd = getcwd();
+
+### START Manual editable settings ###
+# Prepare the connection's options to mysql
 my $dsn = "dbi:mysql:infobot";
+# Change "infobot" to your mysql db name
 my $user = "infobot";
+# mysql user
 my $password = "dpkgdebby";
+# mysql user's password
+
+# Prepare the connection's options to sqlite
+my $dbname = "birba.db";
+# Change the sqlite dbname as needed
+### END Manual editable settings ###
 
 my $schema = app::Schema->connect( $dsn, $user, $password ) or die $DBI::errstr;
+print "Succesfully connected to the mysql database\n";
+
+my $dbh = DBI->connect("dbi:SQLite:dbname=$cwd/$dbname") or die $DBI::errstr;
+$dbh->do('PRAGMA foreign_keys = ON;');
+print "Succesfully connected to the sqlite database $dbname\n";
 
 # Find all of the facts
 my @all_factoids = $schema->resultset('Factoids')->all;
-
-# Cycle through facts and get keys/values
+# Cycle through facts and get keys/values to be imported in the sqlited db
 foreach my $fact (@all_factoids) {
-    print $fact->factoid_key, " = ", $fact->factoid_value, "\n";
+my $key = $fact->factoid_key;
+my $value = $fact->factoid_value;
+
+my $query = $dbh->prepare("INSERT INTO factoids (nick, key, bar1) VALUES (?, ?, ?);");
+$query->execute('roughnecks', $key, $value);
 }
+
+$dbh->disconnect;
 
