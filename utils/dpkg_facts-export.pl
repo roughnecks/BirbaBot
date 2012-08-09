bc4af1c5825ab39673a277b8a65b521a5903bd1a
roughnecks <roughnecks@laltromondo.dynalias.net>
2012-08-09 02:49:38 +0200

    new script to export factoids from a text file back to a sqlite databse

---
+++ utils/dpkg_facts-export.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+# -*- mode: cperl -*-
+
+use warnings;
+use strict;
+
+use Data::Dumper qw(Dumper);
+use DBI;
+use Cwd;
+
+my $cwd = getcwd();
+
+# Prepare the connection's options to sqlite
+my $dbname = "birba.db";
+# Change the sqlite dbname as needed
+### END Manual editable settings ###
+
+my $dbh = DBI->connect("dbi:SQLite:dbname=$cwd/$dbname") or die $DBI::errstr;
+$dbh->do('PRAGMA foreign_keys = ON;');
+print "Succesfully connected to the sqlite database $dbname\n";
+
+open (my $fh, "<", "dpkg_facts") or die $!;
+
+while (my $line = <$fh>) {
+  chomp($line);
+  if ($line =~ m/^\s*(.+)\s,,,\s(.+)$/) {
+    my $query = $dbh->prepare("INSERT INTO factoids (nick, key, bar1) VALUES (?, ?, ?);");
+    $query->execute('roughnecks', $1, $2);
+  } else {print "Probably wrong factoid type: $line\n"}
+}
+
+close($fh);
