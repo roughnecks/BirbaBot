4bf830cb9f9fd4b8238b47f181014e72c1dfeea3
Marco <marco@theanarchistlibrary.org>
2012-08-28 18:17:02 +0200

    Added the debsearch function
      *  edited              BirbaBot/lib/BirbaBot/Debian.pm

--- BirbaBot/lib/BirbaBot/Debian.pm
+++ BirbaBot/lib/BirbaBot/Debian.pm
@@ -33,7 +33,6 @@ my @out = deb_pack_versions($arg,
 
 sub deb_pack_versions {
   my ($arg, $path, $relfiles) = @_;
-  print $arg, ":", $path, ":", $relfiles, "\n";
   return unless (@$relfiles);
   my @out;
   my $pack;
@@ -45,7 +44,7 @@ sub deb_pack_versions {
   foreach my $rel (@$relfiles) {
     next unless ($rel->{rel} and $rel->{url});
     my $file = File::Spec->catfile($path, $rel->{rel});
-    my $result = parse_debfiles($file, $pack);
+    my $result = parse_debfiles($file, $pack, 1);
     if ($result) {
       push @out, $bbold . $rel->{rel} . $ebold . ' => ' . $result;
     }
@@ -58,24 +57,66 @@ sub deb_pack_versions {
 
 =cut
 
-sub parse_debfiles {
-  my ($file, $pack) = @_;
-  return unless $pack;
-  open (my $fh, "<:encoding(utf8)", $file) or die "Could not open $file: $!";
-  my $foundmatch;
-  while (<$fh>) {
-    if (m/^\Q$pack\E\s\((.+)\)\s.+$/) {
-      $foundmatch = $1;
-      last;
-    }
-  }
-  close $fh;
-  return $foundmatch;
-}
 
 sub deb_pack_search {
   my ($arg, $path, $relfiles) = @_;
-  return "To be implemented";
+  my @results;
+  my $pack;
+  if ($arg =~ m/^\s*(\S+)\s*/) {
+    $pack = $1;
+  } else {
+    return "Invalid argument";
+  }
+  foreach my $rel (@$relfiles) {
+    next unless ($rel->{rel} and $rel->{url});
+    my $file = File::Spec->catfile($path, $rel->{rel});
+    push @results, parse_debfiles($file, $pack);
+  }
+  print "Search results: ", @results;
+  my %out;
+  while (@results) {
+    my $pack = shift(@results);
+    $out{$pack} = 1;
+  };
+  @results = sort (keys %out);
+  return "No matches" unless @results;
+  if ((scalar @results) > 50) {
+    splice @results, 49;
+    push @results, "and more...."
+  };
+  return "Found packs: " . join(", ", @results);
 }
+
+
+
+sub parse_debfiles {
+  my ($file, $pack, $exact) = @_;
+  return unless $pack;
+  open (my $fh, "<:encoding(utf8)", $file) or die "Could not open $file: $!";
+  my $foundmatch;
+  my @packs;
+  while (<$fh>) {
+    my $line = $_;
+    if ($exact) {
+      if ($line =~ m/^\Q$pack\E\s\((.+)\)\s.+$/i) {
+	$foundmatch = $1;
+	last;
+      }
+    } else {
+      if ($line =~ m/^(^\S*?\Q$pack\E\S*)\s/i) {
+	print "Found $1 for $pack\n";
+	push @packs, $1;
+      }
+    }
+  }
+  close $fh;
+  if ($exact) {
+    return $foundmatch;
+  } else {
+    return @packs;
+  }
+}
+
+
 
 1;
