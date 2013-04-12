65bc1722df1316ad010a85c338ae19e643e7bf16
roughnecks <roughnecks@laltromondo.dynalias.net>
2013-04-12 10:58:50 +0200

    deleting other unwanted facts by dpkg_facts-import.pl

--- utils/dpkg_facts-import.pl
+++ utils/dpkg_facts-import.pl
@@ -50,8 +50,9 @@ $query->execute('dpkg', $key, $value);
 print "Importing finished.\n";
 
 print "Starting deletion..\n";
+
 # delete non-supported facts
-my $delete = $dbh->prepare('DELETE FROM factoids WHERE key LIKE \'cmd:%\' or bar1 LIKE \'%$randnick%\' or key LIKE \'_default%\';');
+my $delete = $dbh->prepare('DELETE FROM factoids WHERE key LIKE \'cmd:%\' or bar1 LIKE \'%$randnick%\' or key LIKE \'_default%\' or key LIKE \'#del# cmd:%\';');
 $delete->execute();
 print "Deletion finished.\n";
 
