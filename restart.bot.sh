e3317d425e6c9c5ed7e52abd91b2a08c880d0661
roughnecks <roughnecks@laltromondo.dynalias.net>
2013-06-30 14:13:55 +0200

    modified:   restart.bot.sh

--- restart.bot.sh
+++ restart.bot.sh
@@ -1,41 +1,37 @@
 #!/bin/bash
 
-if [ ! -f "$1" ]; then
-    echo "Usage: $0 configurationfile.conf"
+if [[ ! -f "$1" ]]; then
+    printf '%s\n' "Usage: $0 configurationfile.conf"
 fi
 
-botdir=$(dirname $0)
+botdir="$(dirname $0)"
 
-if [ ! -d "$botdir/logs" ]; then
+if [[ ! -d "$botdir/logs" ]]; then
 	mkdir -p "$botdir/logs"
 fi
 
-logfile=$botdir/logs/birba.log
+logfile="$botdir/logs/birba.log"
 
-cd $botdir || exit 2
+cd "$botdir" || exit 2
 
 # the bot never started, as there is no birba.pid, so append to logfile
-if [ ! -f "$botdir/birba.pid" ]; then
-    exec nohup perl birbabot.pl $1 >> "$logfile" 2>&1 &
+if [[ ! -f "$botdir/birba.pid" ]]; then
+    exec nohup perl birbabot.pl "$1" >> "$logfile" 2>&1 &
     exit
 fi
 
-pid=$(cat $botdir/birba.pid)
+pid="$(< $botdir/birba.pid)"
 
-if kill -0 $pid > /dev/null 2>&1 ; then
+if kill -0 "$pid" > /dev/null 2>&1 ; then
     exit
 fi
 
 # rotate the logs
-if [ -f "$logfile" ]; then
-    cat $logfile >> $logfile-`date +%F`;
-    : > $logfile;
+if [[ -f "$logfile" ]]; then
+    cat "$logfile" >> "$logfile-`date +%F`";
+    : > "$logfile";
 fi
 
-echo -n $(date) >> "$logfile"
-echo "Bot restarted by $0" >> "$logfile"
-exec nohup perl birbabot.pl $1 >> "$logfile" 2>&1 &
-
-
-
-
+printf "$(date)\n" >> "$logfile"
+printf '%s\n' "Bot restarted by $0" >> "$logfile"
+exec nohup perl birbabot.pl "$1" >> "$logfile" 2>&1 &
