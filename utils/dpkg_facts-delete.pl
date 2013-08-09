b8e4f04693b7b5669c4ec9b01934c2dd57dbf72e
roughnecks <roughnecks@laltromondo.dynalias.net>
2013-08-09 10:52:49 +0200

    skip deletion of possible garbage from dpkg factoids database as we are getting many false positives

--- utils/dpkg_facts-delete.pl
+++ utils/dpkg_facts-delete.pl
@@ -17,21 +17,6 @@ my $dbh = DBI->connect("dbi:SQLite:dbname=$cwd/$dbname") or die $DBI::errstr;
 $dbh->do('PRAGMA foreign_keys = ON;');
 print "Succesfully connected to the sqlite database $dbname\n";
 
-# delete garbage (thanks skizzhg)
-open (my $fh, "<", "birba-to-delete.txt") or die $!;
-
-while (my $line = <$fh>) {
-  chomp($line);
-  next if ($line =~ m/^#.+$/);
-  next if ($line =~ m/^\s*$/);
-  if ($line =~ m/^\s*(.+)\s,,,\s(.+)$/) {
-    my $query = $dbh->prepare("DELETE FROM factoids WHERE key=? AND nick=?;");
-    $query->execute($1, 'dpkg');
-  } else {print "Probably wrong factoid type: $line\n"}
-}
-
-close($fh);
-
 # delete non-supported facts
 my $delete = $dbh->prepare('DELETE FROM factoids WHERE key LIKE \'cmd:%\' or bar1 LIKE \'%$randnick%\' or key LIKE \'_default%\' or key LIKE \'#del# cmd:%\' or bar1=\'\' or bar1 LIKE \'%#del#%\' or key LIKE \'%#del#%\';');
 $delete->execute();
--- utils/dpkg_facts-mysql-delete.pl
+++ utils/dpkg_facts-mysql-delete.pl
@@ -18,19 +18,6 @@ my $dbh = DBI->connect("dbi:mysql:$db","$user","$passw")
 
 print "Succesfully connected to the mysql database $db\n";
 
-# delete garbage (thanks skizzhg)
-open (my $fh, "<", "birba-to-delete.txt") or die $!;
-
-while (my $line = <$fh>) {
-  chomp($line);
-  next if ($line =~ m/^#.+$/);
-  next if ($line =~ m/^\s*$/);
-  if ($line =~ m/^\s*(.+)\s,,,\s(.+)$/) {
-      my $query = $dbh->prepare("DELETE FROM factoids WHERE factoid_key=?;");
-      $query->execute($1);
-  } else {print "Probably wrong factoid type: $line\n"}
-}
-
 # delete non-supported facts
 my $delete = $dbh->prepare('DELETE FROM factoids WHERE factoid_key LIKE \'cmd:%\' or factoid_value LIKE \'%$randnick%\' or factoid_key LIKE \'_default%\' or factoid_key LIKE \'#del# cmd:%\' or factoid_value=\'\' or factoid_value LIKE \'%#del#%\' or factoid_key LIKE \'%#del#%\';');
 $delete->execute();
